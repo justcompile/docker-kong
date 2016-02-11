de71a7139f2d221f540824f5fc81e351c560948b
justcompile <justcompile@users.noreply.github.com>
2016-02-11 14:51:27 +0000

    Update build.sh

--- kong/build.sh
+++ kong/build.sh
@@ -4,6 +4,6 @@ set -ex
 cd /tmp
 apt-get update
 apt-get install -y netcat lua5.1 openssl libpcre3 dnsmasq curl sudo gettext dnsutils
-curl -L https://github.com/Mashape/kong/releases/download/0.4.1/kong-0.4.1.wheezy_all.deb > kong-0.4.1.wheezy_all.deb
-dpkg -i kong-0.4.1.*.deb
+curl -L https://github.com/Mashape/kong/releases/download/0.6.1/kong-0.6.1.wheezy_all.deb > kong-0.6.1.wheezy_all.deb
+dpkg -i kong-0.6.1.*.deb
 rm /tmp/*
