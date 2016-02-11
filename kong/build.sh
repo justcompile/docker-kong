c56403feecbc3fb1c1d17c5bdbb4a795a09c15c2
Richard Hayes <richard.hayes@storystream.it>
2016-02-11 15:20:09 +0000

    added package dependency due to increase version of kong

--- kong/build.sh
+++ kong/build.sh
@@ -3,7 +3,7 @@ set -ex
 
 cd /tmp
 apt-get update
-apt-get install -y netcat lua5.1 openssl libpcre3 dnsmasq curl sudo gettext dnsutils
+apt-get install -y netcat lua5.1 openssl libpcre3 dnsmasq curl sudo gettext dnsutils procps
 curl -L https://github.com/Mashape/kong/releases/download/0.6.1/kong-0.6.1.wheezy_all.deb > kong-0.6.1.wheezy_all.deb
 dpkg -i kong-0.6.1.*.deb
 rm /tmp/*
